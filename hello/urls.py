5c93e89a330d01ab9cc5ddf9c871c3aa6a62b8d1
namwon94 <namwon94@gmail.com>
2020-03-13 10:33:25 +0900

    test_django1

---
+++ hello/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path("", views.home, name="home")
+]
+
+
+
+
+
+
+
---
+++ hello/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse("Hello, Django!")
+
+from django.http import JsonResponse
+
+def home(request):
+    name = request.GET['name']
+    age = request.GET['age']
+    getRequestDict = request.GET;
+    return JsonResponse(getRequestDict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
